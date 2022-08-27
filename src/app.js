dd40266c4b7c698fbd74a80e6ac338d3e320f80a
bder00s <bonne-deroos@hotmail.com>
2022-08-27 17:21:32 +0200

    verbeteringen + toevoegingen

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import {isUndefined} from "../dist/index.d56a3cb1";
+
 
 //Verwijzingen naar de HTML elementen
 const searchForm = document.getElementById("search-form");
@@ -23,9 +23,9 @@ function searchCountry(e) {
 
 
 // De Async functie die de data ophaalt
-async function fetchCountryDetails (name) {
+async function fetchCountryDetails(name) {
     try {
-        const result = await axios.get(` https://restcountries.com/v2/name/${name} `);
+        const result = await axios.get(`https://restcountries.com/v2/name/${name}`);
         const country = result.data[0];
         console.log(result.data[0]);
 
@@ -33,32 +33,28 @@ async function fetchCountryDetails (name) {
         infoBox.innerHTML = `
 <div id="nameAndFlag">
 <span> <img id="currentFlag" src="${country.flag}" alt="flag" width="50px" /> </span>
-<h2>${country.name}</h2>
+<h2> ${country.name}</h2>
 </div>
-
-<p>${country.name} is situated in ${country.subregion}.</p>
-<p>It has a population of ${country.population}.</p>
- <p>The capital is ${country.capital} ${currencyDescription(country.currencies)}</p>
+<ul>
+<li>${country.name} is situated in ${country.subregion}.</li>
+<li>It has a population of ${country.population}. ${languageLoader(country.languages)}</li>
+ <li>The capital is ${country.capital} ${currencyDescription(country.currencies)}</li>
+ <ul>
 
     `
 
     } catch (error) {
-        if (fetchCountryDetails(name) === undefined) {
-            document.getElementById("search-form");
-            searchForm.innerHTML = "Dit land bestaat niet"
-        }
+        // if (fetchCountryDetails(name) === undefined) {
+        //     document.getElementById("search-form");
+        //     searchForm.innerHTML = "Dit land bestaat niet"
+        // }
         console.error(error)
 
     }
 
 }
 
-
-
-
-
 fetchCountryDetails(name);
-
 
 
 //Functie die het aantal currencies uit de bijbehorende data haalt
@@ -66,12 +62,28 @@ fetchCountryDetails(name);
 function currencyDescription(currencies) {
     let output = 'and you can pay with ';
 
-    if(currencies.length === 2) {
+    if (currencies.length === 2) {
         return output + `${currencies[0].name} and ${currencies[1].name}`
     }
     return output + `${currencies[0].name}`;
 }
 
+
+/// Functie die het aantal landen laadt
+function languageLoader(languages) {
+    let output = 'They speak ';
+
+    if (languages.length === 3) {
+        return output + `${languages[0].name}, ${languages[1].name} and ${languages[2].name}`
+    } else if (languages.length === 2) {
+        return output + `${languages[0].name} and ${languages[1].name}`
+    } else {
+        return output + `${languages[0].name}`;
+    }
+}
+
+
+// ` https://restcountries.com/v2/name/${name}`
 
 
 
